72e46d60c091a5a7644e55d40f8f21cd1791a8a2
harish <harishv98@gmail.com>
2018-09-03 23:41:39 +0530

    subject Name

--- app/components/form-fields.js
+++ app/components/form-fields.js
@@ -1,7 +1,6 @@
 import Component from '@ember/component';
 import {dept} from '../client-data/form-data';
 import {semester} from '../client-data/form-data';
-import {subject} from '../client-data/form-data';
 
 export default Component.extend({
     classNames:['form-transition'],
@@ -10,9 +9,8 @@ export default Component.extend({
     depts:dept,
     semesters:semester,
     didInsertElement(){
-        // this.$(.)
         this.$('.dropdown').dropdown();
-        $('.card').dimmer({
+        this.$('.blurring').dimmer({
             on: 'hover'
           });
     },
@@ -21,18 +19,12 @@ export default Component.extend({
         return (this.get('deptName')!="" && this.get('semesterNum')!="")?true:false;
     },
 
-    fetchSubjects(){
-        // Ember.$.ajax();
-        this.set('subjectData',subject);
-        this.set('')
-        // $('.form-transition').transition('fly left');
-    },
-
     actions:{
         fetchData(data,dropName){
             this.set(`${dropName}`,data);          
             if(this.checkValid()){   
-                this.fetchSubjects();
+                console.log(this.get('subjectData'));
+                this.sendAction('fetchSubjects',this.get('deptName'),this.get('semesterNum'));
             }
         }
     }
--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,21 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+
+    subjectData:[],
+    model(){
+        var model={};
+        model.subjectData=this.get('subjectData');
+        return model;
+    },
+
+    actions:{
+        fetchSubjects(dept,sem){
+            let self = this;
+            this.store.queryRecord('subject',{dept,sem}).then(function(res) {
+                self.set('subjectData',res.subjectNames);
+                self.refresh();
+            })
+        }
+    }
 });
