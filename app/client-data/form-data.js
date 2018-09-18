ba8e3e708d03e1b135efb7020c7994d903a58afe
harishv98 <harishv98@gmail.com>
2018-09-18 15:39:05 +0530

    lab commit

--- app/client-data/form-data.js
+++ app/client-data/form-data.js
@@ -1,100 +1,16 @@
 export const dept = ['CSE', 'EEE', 'MECH', 'CIVIL', 'ECE', 'IT'];
-export const semester = ['sem 1', 'sem 2', 'sem 3', 'sem 4', 'sem 5', 'sem 6', 'sem 7', 'sem 8',]
-export const formData = {
-    twoMark: {
-        unit1: [{
-            content: "This is a two mark"
-        },
-        {
-            content: "this is another two mark"
-        }],
-        unit2: [{
-            content: "This is a two mark"
-        },
-        {
-            content: "this is another two mark"
-        }],
-        unit3: [{
-            content: "This is a two mark"
-        },
-        {
-            content: "this is another two mark"
-        }],
-        unit4: [{
-            content: "This is a two mark"
-        },
-        {
-            content: "this is another two mark"
-        }],
-        unit5: [{
-            content: "This is a two mark"
-        },
-        {
-            content: "this is another two mark"
-        }]
-    },
-    eightMark: {
-        unit1: [{
-            content: "This is a eight mark"
-        },
-        {
-            content: "this is another eight mark"
-        }],
-        unit2: [{
-            content: "This is a eight mark"
-        },
-        {
-            content: "this is another eight mark"
-        }],
-        unit3: [{
-            content: "This is a eight mark"
-        },
-        {
-            content: "this is another eight mark"
-        }],
-        unit4: [{
-            content: "This is a eight mark"
-        },
-        {
-            content: "this is another eight mark"
-        }],
-        unit5: [{
-            content: "This is a eight mark"
-        },
-        {
-            content: "this is another eight mark"
-        }]
-    },
-    sixteenMark: {
-        unit1: [{
-            content: "This is a sixteen mark"
-        },
-        {
-            content: "this is another sixteen mark"
-        }],
-        unit2: [{
-            content: "This is a sixteen mark"
-        },
-        {
-            content: "this is another sixteen mark"
-        }],
-        unit3: [{
-            content: "This is a sixteen mark"
-        },
-        {
-            content: "this is another sixteen mark"
-        }],
-        unit4: [{
-            content: "This is a sixteen mark"
-        },
-        {
-            content: "this is another sixteen mark"
-        }],
-        unit5: [{
-            content: "This is a sixteen mark"
-        },
-        {
-            content: "this is another eight mark"
-        }]
-    }
-}
+export const semester = ['sem 1', 'sem 2', 'sem 3', 'sem 4', 'sem 5', 'sem 6', 'sem 7', 'sem 8', ]
+
+export const questContent = [{
+  content: "this is a question with content of 2 marks"
+}, {
+  content: "this is a question with content of 2 marks"
+}, {
+  content: "this is a question with content of 2 marks"
+}, {
+  content: "this is a question with content of 2 marks"
+}, {
+  content: "this is a question with content of 2 marks"
+}, {
+  content: "this is a question with content of 2 marks"
+}]
--- app/components/fill-questions.js
+++ app/components/fill-questions.js
@@ -1,9 +1,34 @@
 import Component from '@ember/component';
+import  Ember  from 'ember';
+import {questContent} from '../client-data/form-data'
 
 export default Component.extend({
+    subjectUnits:['Unit-1','Unit-2','Unit-3','Unit-4','Unit-5'],
+    twoMark:['ques'],
     actions:{
         add(part){
             $('.ui.longer.modal').modal('show');
+            this.set('questContent',questContent);
+        },
+        fetchQuestion(unit){
+            var url = '';
+            Ember.$.ajax({
+                url: url,
+                type: 'GET',
+                accepts: 'application/json',
+                success: function(data) {
+                    
+                },
+                error: function() {
+                    console.log('DEBUG: GET Enquiries Failed');
+                }
+            });
+        },
+        addQuestion(content){
+            let twoMarks = this.get('twoMark');
+            console.log(twoMarks);
+            twoMarks.push(content);
+            this.set('twoMark',twoMarks);
         }
     }
 });
